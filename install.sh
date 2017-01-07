0894529fa14944c99cdab7cd598c2d4722428777
SylvioT <sylvio.tavares@hotmail.com>
2017-01-07 00:40:51 -0300

    Modified install

--- install.sh
+++ install.sh
@@ -2,19 +2,21 @@
 
 fdisk -l
 
-mkfs.ext4 -c /dev/sdaR
+mkfs.ext4 -c /dev/sda3
 
 mkswap --check /dev/sdaS
 swapon /dev/sdaS
 
-mount /dev/sdaR /mnt
-mount /dev/sdaB /mnt/boot
+mount /dev/sda3 /mnt
+#mount /dev/sdaB /mnt/boot
 
 #descomentando o mirrorlist do Brazil
 
+pacstrap -i /mnt base-devel
+
 genfstab -U /mnt > /mnt/etc/fstab
 
-arch-chroot /mnt
+arch-chroot /mnt /bin/bash
 
 echo "en_US.UTF-8" > /etc/locale.gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
@@ -23,15 +25,17 @@ export LANG=en_US.UTF-8
 ln -s /usr/share/zoneinfo/America/Bahia > /etc/localtime
 hwclock --systohc --utc
 
-pacman -S grub os-prober
+pacman -Sy
+pacman -S --noconfirm grub os-prober
 
 grub-install --recheck --target=i386-pc /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 
 echo sylviot > /etc/hostname
-passwd
+# passwd
 
 exit
 
 umount -R /mnt
-reboot
+echo "END"
+#reboot
