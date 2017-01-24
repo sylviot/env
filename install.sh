1a213fe25cdb98a8d6075d0c7e4801f43c14a376
SylvioT <sylvio.tavares@hotmail.com>
2017-01-23 23:32:51 -0300

    order change

--- install.sh
+++ install.sh
@@ -10,9 +10,9 @@ swapon /dev/sda2
 
 mount /dev/sda3 /mnt
 
-rm -f /etc/pacman.d/mirrorlist
-curl -s "https://www.archlinux.org/mirrorlist/?country=BR&country=US&protocol=http&protocol=https&use_mirror_status=on" > /etc/pacman.d/mirrorlist
-sed -i 's/\#Server/Server/g' /etc/pacman.d/mirrorlist
+#rm -f /etc/pacman.d/mirrorlist
+#curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=http&protocol=https&use_mirror_status=on" > /etc/pacman.d/mirrorlist
+#sed -i 's/\#.*(Server)/\1/g' /etc/pacman.d/mirrorlist
 
 pacstrap /mnt base base-devel
 
@@ -20,12 +20,14 @@ genfstab -U /mnt > /mnt/etc/fstab
 
 cat <<EOF > /mnt/script.sh
 
+pacman -Sy --noconfirm grub os-prober sudo
+
 echo "Configuration hostname..."
 echo "sylviot" > /etc/hostname
-passwd sylviot
 
 useradd -m -G wheel,users -s /bin/bash sylviot
-sed -i -r 's/^#.(%wheel.[^PSWD]*$)/\1/' /etc/sudores
+sed -i -r 's/^#.(%wheel.[^PSWD]*$)/\1/' /etc/sudoers
+passwd sylviot
 
 locale >/etc/locale.conf
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
@@ -38,8 +40,6 @@ hwclock --systohc --utc
 timedatectl set-ntp true
 
 systemctl enable dhcpcd
-
-pacman -Sy --noconfirm grub os-prober
 
 grub-install --recheck --target=i386-pc /dev/sda
 sed -i -r 's/(^.*_TIMEOUT=)5/\10/' /etc/default/grub
