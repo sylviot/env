30223c9a9222355f24057c40d296f5f47e97ea6f
Sylvio Tavares <sylvio.tavares@hotmail.com>
2019-02-28 11:30:48 -0400

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -57,7 +57,7 @@ configure_desktop() {
   sudo systemctl enable lightdm
   
   # Download Rele Theme
-  ~#curl -o /tmp/rele.tar.bz2 https://dl.opendesktop.org/api/files/downloadfile/id/1462392025/s/947eec5cfb08c2bb291190370343f799/t/1527263464/u/60272/77260-rele-xfce4.tar.bz2
+  #curl -o /tmp/rele.tar.bz2 https://dl.opendesktop.org/api/files/downloadfile/id/1462392025/s/947eec5cfb08c2bb291190370343f799/t/1527263464/u/60272/77260-rele-xfce4.tar.bz2
   #tar -xjvf /tmp/rele.tar.bz2
   #sudo cp -R /tmp/Rele /usr/share/themes/Rele
 
