16c469317857d146f58f2d2c1dca67fa21c0ce08
sylviot <sylvio.tavares@hotmail.com>
2019-11-12 23:24:00 -0500

    git config

--- manjaro.sh
+++ manjaro.sh
@@ -45,7 +45,7 @@ base() {
   print "> Configuration base..."
 
   with_pacman "xf86-input-mouse xf86-input-keyboard xf86-video-ati"
-  with_pacman "bash-completion htop unrar zsh wget"
+  with_pacman "bash-completion htop unrar wget"
 }
 
 desktop() {
@@ -62,7 +62,7 @@ desktop() {
 development() {
   print "> Configuration development..."
 
-  with_pacman 'git vim docker chromium'
+  with_pacman 'chromium ctags docker git vim zsh'
   with_yaourt "google-chrome"
 
   if [ -n "`(systemctl is-enabled docker) | grep disabled`" ]; then
@@ -73,9 +73,29 @@ development() {
 
   sudo ln -s ~/env/bin/* /usr/local/bin/ 2>/dev/null
 
-  print "Install dotnet..."
-  wget https://dot.net/v1/dotnet-install.sh -O /tmp/dotnet-install.sh
-  sudo sh /tmp/dotnet-install.sh --install-dir /opt/dotnet -channel Current -version latest
+  if [ -n "`(hash dotnet 2>/dev/null) | grep not\ found`" ]; then
+    print "Install dotnet..."
+    wget https://dot.net/v1/dotnet-install.sh -O /tmp/dotnet-install.sh
+    sudo sh /tmp/dotnet-install.sh --install-dir /opt/dotnet -channel Current -version latest
+    sudo ln -s /opt/dotnet /usr/local/bin 2>/dev/null
+  fi
+
+  if [ -z "`(git config --list)` | grep user" ]; then
+    git config --global user.name "sylviot"
+    git config --global user.email "sylvio.tavares@hotmail.com"
+  fi
+
+  if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    print "Configuring oh-my-zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    # git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+    cp $HOME/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+  fi
+  
+  if [ ! -z "`echo $SHELL | grep zsh`" ]; then
+    print "Configuring zsh..."
+    sudo chsh -s $(which zsh)
+  fi
 }
 
 vim() {
